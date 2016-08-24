5f1ecb9e96c911d4d439945db87eb4eea61d3c38
Mads Kristensen <post@madskristensen.net>
2016-08-24 14:01:23 -0700

    Updates

---
+++ src/Vsix/VSPackage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace SnippetPackVsix
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
+    [Guid(PackageGuidString)]
+    public sealed class VSPackage : Package
+    {
+        public const string PackageGuidString = "cb581b54-f056-4eca-b408-0a8608b615ae";
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
