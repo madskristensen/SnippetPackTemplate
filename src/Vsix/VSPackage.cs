93e6d92fac0d5e070ebd042f76fc5d56f4fe1976
Mads Kristensen <post@madskristensen.net>
2016-09-09 12:12:28 -0700

    Cleaned up package class

--- src/Vsix/VSPackage.cs
+++ src/Vsix/VSPackage.cs
@@ -4,13 +4,11 @@ using Microsoft.VisualStudio.Shell;
 
 namespace SnippetPackVsix
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
-    [Guid(PackageGuidString)]
+    [Guid("cb581b54-f056-4eca-b408-0a8608b615ae")]
     public sealed class VSPackage : Package
     {
-        public const string PackageGuidString = "cb581b54-f056-4eca-b408-0a8608b615ae";
-
         protected override void Initialize()
         {
             base.Initialize();
